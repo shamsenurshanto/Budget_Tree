99a68bf4e6f207be07d4c5cb89787aa7a962c1e4
shamsenurshanto <shamsenurshanto008@gmail.com>
2021-10-02 17:18:04 +0600

    added

--- src/Component/Showtree/Showtree.js
+++ src/Component/Showtree/Showtree.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './Showtree.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faCartPlus} />
+
+
 const Showtree = (props) => {
-    const {name , img , intro } = props.tree;
+    const {name , img , intro , price ,starCount } = props.tree;
     console.log(props)
     return (
         <div className="showtreesClass col-lg-4">
                <p>this is {name}</p>
                 <img src={img}></img>
                 <p>{intro}</p>
+                <h5>Price : {price}</h5>
+                <h5>Star Count : {starCount}</h5>
+                <button className="btn btn btn-outline-success" > {element} click to ADD </button>
+                
                 
            
         </div>
